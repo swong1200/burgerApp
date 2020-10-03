fab4a6ae869bf9c378741c4d516b7cbed7dcd140
Stuart Wong <swong31@gmail.com>
2020-10-03 16:04:22 -0700

    Updated for jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
-
+let connection;
 if (process.env.JAWSDB_URL) {
-  let connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  let connection = mysql.createConnection({
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "jurassic5",
     database: "burgers_db",
   });
 }
